1c4113d3915d0d2d854a6888f0c07260bc089a20
George <gwmkwan@gmail.com>
2022-12-18 14:31:31 -0700

    chore: initialize project

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click Here!!!"></Button>
       <StatusBar style="auto" />
     </View>
   );
