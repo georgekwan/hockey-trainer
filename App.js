22052890e0af4e60e337c0220ac063145d133217
George <gwmkwan@gmail.com>
2023-01-12 20:07:30 -0700

    feat: add home screen

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { theme } from './src/core/theme';
 import {
-    StartScreen,
-    LoginScreen,
-    RegisterScreen,
-    ResetPasswordScreen,
-    Dashboard,
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+  HomeScreen,
 } from './src/screens';
 import { FirebaseProvider } from './src/providers/FirebaseProvider.js';
 import { AuthProvider } from './src/providers/AuthProvider.js';
@@ -16,41 +17,33 @@ import { AuthProvider } from './src/providers/AuthProvider.js';
 const Stack = createStackNavigator();
 
 export default function App() {
-    return (
-        <FirebaseProvider>
-            <AuthProvider>
-                <Provider theme={theme}>
-                    <NavigationContainer>
-                        <Stack.Navigator
-                            initialRouteName='StartScreen'
-                            screenOptions={{
-                                headerShown: false,
-                            }}
-                        >
-                            <Stack.Screen
-                                name='StartScreen'
-                                component={StartScreen}
-                            />
-                            <Stack.Screen
-                                name='LoginScreen'
-                                component={LoginScreen}
-                            />
-                            <Stack.Screen
-                                name='RegisterScreen'
-                                component={RegisterScreen}
-                            />
-                            <Stack.Screen
+  return (
+    <FirebaseProvider>
+      <AuthProvider>
+        <Provider theme={theme}>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="StartScreen"
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Stack.Screen name="StartScreen" component={StartScreen} />
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+              <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+              {/* <Stack.Screen
                                 name='Dashboard'
                                 component={Dashboard}
-                            />
-                            <Stack.Screen
-                                name='ResetPasswordScreen'
-                                component={ResetPasswordScreen}
-                            />
-                        </Stack.Navigator>
-                    </NavigationContainer>
-                </Provider>
-            </AuthProvider>
-        </FirebaseProvider>
-    );
+                            /> */}
+              <Stack.Screen name="HomeScreen" component={HomeScreen} />
+              <Stack.Screen
+                name="ResetPasswordScreen"
+                component={ResetPasswordScreen}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+      </AuthProvider>
+    </FirebaseProvider>
+  );
 }
