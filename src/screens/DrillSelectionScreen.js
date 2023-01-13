60b1feec6099f4c9c921ec52a7e9d986b5f11122
George <gwmkwan@gmail.com>
2023-01-12 21:22:13 -0700

    feat: create drill selection screen

---
+++ src/screens/DrillSelectionScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const DrillSelectionScreen = () => {
+  return (
+    <View>
+      <Text>DrillSelectionScreen</Text>
+    </View>
+  );
+};
+
+export default DrillSelectionScreen;
+
+const styles = StyleSheet.create({});
