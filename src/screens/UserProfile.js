5405d4636da13d86944fe0fb505b13beebe9d242
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2023-01-13 08:10:44 -0700

    refactor: edit data-table from RN Paper

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { DataTable } from 'react-native-paper';
+
+// const optionsPerPage = [2, 3, 4];
 
 const UserProfile = () => {
-  return (
-    <View>
-      <Text>UserProfile</Text>
-    </View>
-  )
-}
+    // const [page, setPage] = React.useState < number > 0;
+    // const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
 
-export default UserProfile
+    // React.useEffect(() => {
+    //     setPage(0);
+    // }, [itemsPerPage]);
 
-const styles = StyleSheet.create({})
+    return (
+        <DataTable>
+            <DataTable.Header>
+                <DataTable.Title>Dessert</DataTable.Title>
+                <DataTable.Title numeric>Calories</DataTable.Title>
+                <DataTable.Title numeric>Fat</DataTable.Title>
+            </DataTable.Header>
+
+            <DataTable.Row>
+                <DataTable.Cell>Frozen yogurt</DataTable.Cell>
+                <DataTable.Cell numeric>159</DataTable.Cell>
+                <DataTable.Cell numeric>6.0</DataTable.Cell>
+            </DataTable.Row>
+
+            <DataTable.Row>
+                <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+                <DataTable.Cell numeric>237</DataTable.Cell>
+                <DataTable.Cell numeric>8.0</DataTable.Cell>
+            </DataTable.Row>
+
+            {/* <DataTable.Pagination
+                page={page}
+                numberOfPages={3}
+                onPageChange={(page) => setPage(page)}
+                label='1-2 of 6'
+                optionsPerPage={optionsPerPage}
+                itemsPerPage={itemsPerPage}
+                setItemsPerPage={setItemsPerPage}
+                showFastPagination
+                optionsLabel={'Rows per page'}
+            /> */}
+        </DataTable>
+    );
+};
+
+export default UserProfile;
+
+const styles = StyleSheet.create({});
