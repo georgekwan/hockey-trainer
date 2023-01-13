b9cbe992c0274ed3ccc0a4e2ac2fd5ad856bd5c8
George <gwmkwan@gmail.com>
2023-01-13 08:49:46 -0700

    fix: change back to original

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,118 +9,116 @@ const HEIGHT = Dimensions.get('screen').height;
 const HomeScreen = () => (
   <View style={styles.container}>
     <Card style={styles.card}>
-      <View>
-        <Card.Content>
-          <View>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.08,
-                fontWeight: 'bold',
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.02,
-              }}
-            >
-              Welcome, Josh!
-            </Text>
-          </View>
-          <View style={{ flexDirection: 'row' }}>
-            <Avatar.Icon size={100} icon="face-man-shimmer" />
-            <View
-              style={{
-                paddingLeft: 10,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: WIDTH * 0.08,
-                  fontWeight: '800',
-                  paddingLeft: 5,
-                }}
-              >
-                LAST TRAINING
-              </Text>
-              <Text
-                style={{
-                  fontSize: WIDTH * 0.05,
-                  fontWeight: '500',
-                  paddingLeft: 5,
-                  paddingVertical: WIDTH * 0.02,
-                }}
-              >
-                Mom's cookies pattern
-              </Text>
-            </View>
-          </View>
+      <Card.Content>
+        <View>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.08,
+              fontWeight: 'bold',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
+          >
+            Welcome, Josh!
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Avatar.Icon size={100} icon="face-man-shimmer" />
           <View
-            style={
-              ({ flexDirection: 'column' },
-              { justifyContent: 'space-around' },
-              { margin: WIDTH * 0.005 })
-            }
+            style={{
+              paddingLeft: 10,
+            }}
           >
             <Text
               style={{
                 fontSize: WIDTH * 0.08,
                 fontWeight: '800',
                 paddingLeft: 5,
+              }}
+            >
+              LAST TRAINING
+            </Text>
+            <Text
+              style={{
+                fontSize: WIDTH * 0.05,
+                fontWeight: '500',
+                paddingLeft: 5,
                 paddingVertical: WIDTH * 0.02,
               }}
             >
-              OVERALL STATS
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: '500',
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              <List.Icon icon="bullseye-arrow" color={theme.colors.primary} />
-              Average accuracy: 88%
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: '500',
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              <List.Icon icon="target" color={theme.colors.primary} />
-              Total shots taken: 165
+              Mom's cookies pattern
             </Text>
           </View>
-          <View
-            style={
-              ({ flexDirection: 'column' },
-              { justifyContent: 'space-around' },
-              { margin: WIDTH * 0.005 })
-            }
+        </View>
+        <View
+          style={
+            ({ flexDirection: 'column' },
+            { justifyContent: 'space-around' },
+            { margin: WIDTH * 0.005 })
+          }
+        >
+          <Text
+            style={{
+              fontSize: WIDTH * 0.08,
+              fontWeight: '800',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
           >
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: '800',
-                // paddingLeft: 5,
-                paddingVertical: WIDTH * 0.02,
-              }}
-            >
-              RECOMMENDED PATTERN:
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: '500',
-                // paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              Top-Left
-            </Text>
-          </View>
-        </Card.Content>
-      </View>
+            OVERALL STATS
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            <List.Icon icon="bullseye-arrow" color={theme.colors.primary} />
+            Average accuracy: 88%
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            <List.Icon icon="target" color={theme.colors.primary} />
+            Total shots taken: 165
+          </Text>
+        </View>
+        <View
+          style={
+            ({ flexDirection: 'column' },
+            { justifyContent: 'space-around' },
+            { margin: WIDTH * 0.005 })
+          }
+        >
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '800',
+              // paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
+          >
+            RECOMMENDED PATTERN:
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              // paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            Top-Left
+          </Text>
+        </View>
+      </Card.Content>
     </Card>
   </View>
 );
