9c60a7ff059ee5103c0cd98ed7bc00b1e69ccd8b
George <gwmkwan@gmail.com>
2023-01-14 16:19:27 -0700

    feat: setup basic drill screen

--- src/screens/DrillSelectionScreen.js
+++ src/screens/DrillSelectionScreen.js
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+const WIDTH = Dimensions.get('screen').width;
+const HEIGHT = Dimensions.get('screen').height;
 
 const DrillSelectionScreen = () => {
   return (
     <View>
-      <Text>DrillSelectionScreen</Text>
+      <View>
+        <Text>DrillSelectionScreen</Text>
+      </View>
+      <View>
+        <View>
+          <Text>Drill One</Text>
+        </View>
+        <View>
+          <Text>Drill Two</Text>
+        </View>
+        <View>
+          <Text>Drill Three</Text>
+        </View>
+        <View>
+          <Text>Drill Four</Text>
+        </View>
+        <View>
+          <Text>Drill Fire</Text>
+        </View>
+        <View>
+          <Text>Drill Six</Text>
+        </View>
+      </View>
+
+      <View
+        style={
+          ({ flexDirection: 'row' },
+          { margin: WIDTH * 0.005 },
+          { paddingVertical: WIDTH * 0.001 })
+        }
+      >
+        <View>
+          <Button mode="contained" onPress={() => console.log('Pressed')}>
+            3 SEC
+          </Button>
+        </View>
+        <View>
+          <Button mode="contained" onPress={() => console.log('Pressed')}>
+            5 SEC
+          </Button>
+        </View>
+        <View>
+          <Button mode="contained" onPress={() => console.log('Pressed')}>
+            7 SEC
+          </Button>
+        </View>
+      </View>
+      <View style={{ margin: WIDTH * 0.025 }}>
+        <Button
+          mode="contained"
+          onPress={() => console.log('Pressed')}
+          style={{ borderRadius: 25 }}
+          labelStyle={[styles.fontStyles, styles.md3FontStyles]}
+        >
+          TRAIN NOW
+        </Button>
+      </View>
     </View>
   );
 };
 
 export default DrillSelectionScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  md3FontStyles: {
+    lineHeight: 32,
+  },
+  fontStyles: {
+    fontWeight: '800',
+    fontSize: 24,
+  },
+});
