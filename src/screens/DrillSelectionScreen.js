6912f97a6f081250bc2b2633a833582633ba2379
George <gwmkwan@gmail.com>
2023-01-14 17:41:29 -0700

    style: adjust select pattern positioning

--- src/screens/DrillSelectionScreen.js
+++ src/screens/DrillSelectionScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, List } from 'react-native-paper';
 
 const WIDTH = Dimensions.get('screen').width;
 const HEIGHT = Dimensions.get('screen').height;
@@ -31,19 +31,44 @@ const DrillSelectionScreen = () => {
           <Text>Drill Six</Text>
         </View>
       </View>
+      <View
+        style={[
+          { flexDirection: 'row' },
+          { alignItems: 'center' },
+          { justifyContent: 'center' },
+        ]}
+      >
+        <Text
+          style={{
+            fontSize: WIDTH * 0.08,
+            fontWeight: '800',
+
+            paddingVertical: WIDTH * 0.02,
+          }}
+        >
+          <List.Icon icon="timer" color="blue" />
+          SELECT PATTERN
+        </Text>
+      </View>
 
       <View
         style={[
           { flexDirection: 'row' },
           { alignItems: 'center' },
           { justifyContent: 'space-between' },
-          { margin: WIDTH * 0.005 },
+          { margin: WIDTH * 0.015 },
         ]}
       >
         <Button
           mode="contained"
           onPress={() => console.log('Pressed')}
-          style={[{ width: WIDTH * 0.3 }, { borderRadius: 10 }]}
+          style={[
+            { width: WIDTH * 0.3 },
+            { height: HEIGHT * 0.08 },
+            { alignItems: 'center' },
+            { justifyContent: 'center' },
+            { borderRadius: 10 },
+          ]}
         >
           3 SEC
         </Button>
@@ -51,7 +76,13 @@ const DrillSelectionScreen = () => {
         <Button
           mode="contained"
           onPress={() => console.log('Pressed')}
-          style={[{ width: WIDTH * 0.3 }, { borderRadius: 10 }]}
+          style={[
+            { width: WIDTH * 0.3 },
+            { height: HEIGHT * 0.08 },
+            { alignItems: 'center' },
+            { justifyContent: 'center' },
+            { borderRadius: 10 },
+          ]}
         >
           5 SEC
         </Button>
@@ -59,20 +90,40 @@ const DrillSelectionScreen = () => {
         <Button
           mode="contained"
           onPress={() => console.log('Pressed')}
-          style={[{ width: WIDTH * 0.3 }, { borderRadius: 10 }]}
+          style={[
+            { width: WIDTH * 0.3 },
+            { height: HEIGHT * 0.08 },
+            { alignItems: 'center' },
+            { justifyContent: 'center' },
+            { borderRadius: 10 },
+          ]}
         >
           7 SEC
         </Button>
       </View>
       <View style={{ margin: WIDTH * 0.025 }}>
-        <Button
-          mode="contained"
-          onPress={() => console.log('Pressed')}
-          style={{ borderRadius: 25 }}
-          labelStyle={[styles.fontStyles, styles.md3FontStyles]}
+        <View
+          style={[
+            { flexDirection: 'row' },
+            { alignItems: 'center' },
+            { justifyContent: 'center' },
+          ]}
         >
-          TRAIN NOW
-        </Button>
+          <Button
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+            style={[
+              { width: WIDTH * 0.8 },
+              { height: HEIGHT * 0.06 },
+              { alignItems: 'center' },
+              { justifyContent: 'center' },
+              { borderRadius: 25 },
+            ]}
+            labelStyle={[styles.fontStyles, styles.md3FontStyles]}
+          >
+            TRAIN NOW
+          </Button>
+        </View>
       </View>
     </View>
   );
