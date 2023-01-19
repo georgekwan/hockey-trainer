16aa27a069750399647d43967d97229763a7b093
George <gwmkwan@gmail.com>
2023-01-19 11:51:32 -0700

    feat: add harpia logo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,126 +1,136 @@
-import * as React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import { Avatar, Card, Text, List } from "react-native-paper";
-import { theme } from "../core/theme";
+import * as React from 'react';
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
+import { Avatar, Card, Text, List } from 'react-native-paper';
 
-const WIDTH = Dimensions.get("screen").width;
-const HEIGHT = Dimensions.get("screen").height;
+const WIDTH = Dimensions.get('screen').width;
+const HEIGHT = Dimensions.get('screen').height;
 
 const HomeScreen = () => (
   <View style={styles.container}>
+    <View
+      style={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        paddingBottom: HEIGHT * 0.05,
+      }}
+    >
+      <Image
+        source={require('../../assets/harpia-logo.png')}
+        style={{ resizeMode: 'contain', height: HEIGHT * 0.085 }}
+      />
+    </View>
     <Card style={styles.card}>
-      <View>
-        <Card.Content>
-          <View>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.08,
-                fontWeight: "bold",
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.02,
-              }}
-            >
-              Welcome, Josh!
-            </Text>
-          </View>
-          <View style={{ flexDirection: "row" }}>
-            <Avatar.Icon size={100} icon="face-man-shimmer" />
-            <View
-              style={{
-                paddingLeft: 10,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: WIDTH * 0.08,
-                  fontWeight: "800",
-                  paddingLeft: 5,
-                }}
-              >
-                LAST TRAINING
-              </Text>
-              <Text
-                style={{
-                  fontSize: WIDTH * 0.05,
-                  fontWeight: "500",
-                  paddingLeft: 5,
-                  paddingVertical: WIDTH * 0.02,
-                }}
-              >
-                Mom's cookies pattern
-              </Text>
-            </View>
-          </View>
+      <Card.Content>
+        <View>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.08,
+              fontWeight: 'bold',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
+          >
+            Welcome, Josh!
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Avatar.Icon size={100} icon="face-man-shimmer" />
           <View
-            style={
-              ({ flexDirection: "column" },
-              { justifyContent: "space-around" },
-              { margin: WIDTH * 0.005 })
-            }
+            style={{
+              paddingLeft: 10,
+            }}
           >
             <Text
               style={{
                 fontSize: WIDTH * 0.08,
-                fontWeight: "800",
+                fontWeight: '800',
+                paddingLeft: 5,
+              }}
+            >
+              LAST TRAINING
+            </Text>
+            <Text
+              style={{
+                fontSize: WIDTH * 0.05,
+                fontWeight: '500',
                 paddingLeft: 5,
                 paddingVertical: WIDTH * 0.02,
               }}
             >
-              OVERALL STATS
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: "500",
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              <List.Icon icon="bullseye-arrow" color={theme.colors.primary} />
-              Average accuracy: 88%
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: "500",
-                paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              <List.Icon icon="puck" color={theme.colors.primary} />
-              Total shots taken: 165
+              Mom's cookies pattern
             </Text>
           </View>
-          <View
-            style={
-              ({ flexDirection: "column" },
-              { justifyContent: "space-around" },
-              { margin: WIDTH * 0.005 })
-            }
+        </View>
+        <View
+          style={
+            ({ flexDirection: 'column' },
+            { justifyContent: 'space-around' },
+            { margin: WIDTH * 0.005 })
+          }
+        >
+          <Text
+            style={{
+              fontSize: WIDTH * 0.08,
+              fontWeight: '800',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
           >
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: "800",
-                // paddingLeft: 5,
-                paddingVertical: WIDTH * 0.02,
-              }}
-            >
-              RECOMMENDED PATTERN:
-            </Text>
-            <Text
-              style={{
-                fontSize: WIDTH * 0.05,
-                fontWeight: "500",
-                // paddingLeft: 5,
-                paddingVertical: WIDTH * 0.001,
-              }}
-            >
-              Top-Left
-            </Text>
-          </View>
-        </Card.Content>
-      </View>
+            OVERALL STATS
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            <List.Icon icon="bullseye-arrow" color="blue" />
+            Average accuracy: 88%
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            <List.Icon icon="target" color="blue" />
+            Total shots taken: 165
+          </Text>
+        </View>
+        <View
+          style={
+            ({ flexDirection: 'column' },
+            { justifyContent: 'space-around' },
+            { margin: WIDTH * 0.005 })
+          }
+        >
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '800',
+              // paddingLeft: 5,
+              paddingVertical: WIDTH * 0.02,
+            }}
+          >
+            RECOMMENDED PATTERN:
+          </Text>
+          <Text
+            style={{
+              fontSize: WIDTH * 0.05,
+              fontWeight: '500',
+              // paddingLeft: 5,
+              paddingVertical: WIDTH * 0.001,
+            }}
+          >
+            Top-Left
+          </Text>
+        </View>
+      </Card.Content>
     </Card>
   </View>
 );
@@ -130,8 +140,8 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  card: { width: "100%", height: "50%", borderColor: "red", borderWidth: 5 },
+  card: { width: '100%', height: '50%', borderColor: 'red', borderWidth: 5 },
 });
