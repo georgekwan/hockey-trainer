3d2b29cf1e932d76809d1a59af94f3ae263f08b7
George <gwmkwan@gmail.com>
2023-02-08 15:25:00 -0700

    feat: pre-enter dev login setting

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -34,8 +34,8 @@ function LoginScreen({ navigation }) {
   const { login, authErrorMessages } = useContext(AuthContext);
 
   //TODO change email and password back to blank when deploy
-  const [email, setEmail] = useState(''); // input field value cannot be null
-  const [password, setPassword] = useState(''); // input field value cannot be null
+  const [email, setEmail] = useState('george@gmail.com'); // input field value cannot be null
+  const [password, setPassword] = useState('123456'); // input field value cannot be null
 
   const [loginRunning, setLoginRunning] = useState(false);
   const [errorMessage, setErrorMessage] = useState();
