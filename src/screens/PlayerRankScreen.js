3a8a8b587b307dc54e700fcc9f34f881a713609d
George <gwmkwan@gmail.com>
2023-02-13 15:19:20 -0700

    feat: add mock data to table

--- src/screens/PlayerRankScreen.js
+++ src/screens/PlayerRankScreen.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, View } from 'react-native';
 import { DataTable } from 'react-native-paper';
+
+import FullLogo from '../components/FullLogo.js';
+
+const HEIGHT = Dimensions.get('screen').height;
 
 // You can import from local files
 // Cannot use PatternProvider to pull data
@@ -8,21 +12,54 @@ export default function PlayerRankScreen() {
   // const { patternHistory, sortedPatternHistory } = useContext(PatternContext);
 
   return (
-    <ScrollView>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Player</DataTable.Title>
-          <DataTable.Title numeric># of completed drills</DataTable.Title>
-          <DataTable.Title numeric>Accuracy</DataTable.Title>
-        </DataTable.Header>
-        return (
+    <>
+      <View style={styles.logo}>
+        <FullLogo />
+      </View>
+      <ScrollView>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Player</DataTable.Title>
+            <DataTable.Title numeric># of completed drills</DataTable.Title>
+            <DataTable.Title sortDirection="descending" numeric>
+              Accuracy
+            </DataTable.Title>
+          </DataTable.Header>
+          {/* return (
         <DataTable.Row>
           <DataTable.Cell>George</DataTable.Cell>
           <DataTable.Cell numeric>15</DataTable.Cell>
           <DataTable.Cell numeric>80%</DataTable.Cell>
         </DataTable.Row>
-        );
-      </DataTable>
-    </ScrollView>
+        ); */}
+
+          <DataTable.Row>
+            <DataTable.Cell>George</DataTable.Cell>
+            <DataTable.Cell numeric>15</DataTable.Cell>
+            <DataTable.Cell numeric>80</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>Gabe</DataTable.Cell>
+            <DataTable.Cell numeric>10</DataTable.Cell>
+            <DataTable.Cell numeric>82</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>Emmanuel</DataTable.Cell>
+            <DataTable.Cell numeric>11</DataTable.Cell>
+            <DataTable.Cell numeric>79</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+      </ScrollView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    marginTop: HEIGHT * 0.06,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
