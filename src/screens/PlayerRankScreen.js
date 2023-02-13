7325ef7671cd456de5d7dc13a2e2bf72750617a3
George <gwmkwan@gmail.com>
2023-02-13 13:34:10 -0700

    feat: add mock data into table

--- src/screens/PlayerRankScreen.js
+++ src/screens/PlayerRankScreen.js
@@ -1,33 +1,27 @@
 import * as React from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { ScrollView } from 'react-native';
 import { DataTable } from 'react-native-paper';
 
 // You can import from local files
-
+// Cannot use PatternProvider to pull data
 export default function PlayerRankScreen() {
-  const { patternHistory, sortedPatternHistory } = useContext(PatternContext);
+  // const { patternHistory, sortedPatternHistory } = useContext(PatternContext);
 
   return (
     <ScrollView>
       <DataTable>
         <DataTable.Header>
           <DataTable.Title>Player</DataTable.Title>
-          <DataTable.Title>Clan</DataTable.Title>
-          <DataTable.Title>Accuracy</DataTable.Title>
+          <DataTable.Title numeric># of completed drills</DataTable.Title>
+          <DataTable.Title numeric>Accuracy</DataTable.Title>
         </DataTable.Header>
-
-        {sortedPatternHistory?.map((ph, key) => {
-          return (
-            <DataTable.Row key={key}>
-              <DataTable.Cell>{ph.drillId}</DataTable.Cell>
-              <DataTable.Cell numeric>
-                {Math.round(((C.totalShots - ph?.totalMisses) / C.totalShots) * 100)}%
-              </DataTable.Cell>
-              <DataTable.Cell numeric>{convertTimestamp(ph.date?.seconds || 0)}</DataTable.Cell>
-            </DataTable.Row>
-          );
-        })}
+        return (
+        <DataTable.Row>
+          <DataTable.Cell>George</DataTable.Cell>
+          <DataTable.Cell numeric>15</DataTable.Cell>
+          <DataTable.Cell numeric>80%</DataTable.Cell>
+        </DataTable.Row>
+        );
       </DataTable>
     </ScrollView>
   );
