379e83dafce5eef093b52c8d8c9c01e3b5d9ee42
George <gwmkwan@gmail.com>
2023-02-13 15:50:36 -0700

    feat: add mock data

--- src/screens/PlayerRankScreen.js
+++ src/screens/PlayerRankScreen.js
@@ -50,6 +50,56 @@ export default function PlayerRankScreen() {
             <DataTable.Cell numeric>11</DataTable.Cell>
             <DataTable.Cell numeric>79</DataTable.Cell>
           </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Romell</DataTable.Cell>
+            <DataTable.Cell numeric>200</DataTable.Cell>
+            <DataTable.Cell numeric>10</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Abby</DataTable.Cell>
+            <DataTable.Cell numeric>25</DataTable.Cell>
+            <DataTable.Cell numeric>75</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Cobi</DataTable.Cell>
+            <DataTable.Cell numeric>5</DataTable.Cell>
+            <DataTable.Cell numeric>95</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Abneet</DataTable.Cell>
+            <DataTable.Cell numeric>16</DataTable.Cell>
+            <DataTable.Cell numeric>73</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Afshin</DataTable.Cell>
+            <DataTable.Cell numeric>30</DataTable.Cell>
+            <DataTable.Cell numeric>68</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Danielle</DataTable.Cell>
+            <DataTable.Cell numeric>33</DataTable.Cell>
+            <DataTable.Cell numeric>78</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Ashish</DataTable.Cell>
+            <DataTable.Cell numeric>8</DataTable.Cell>
+            <DataTable.Cell numeric>89</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Jennifer</DataTable.Cell>
+            <DataTable.Cell numeric>3</DataTable.Cell>
+            <DataTable.Cell numeric>84</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Vince</DataTable.Cell>
+            <DataTable.Cell numeric>23</DataTable.Cell>
+            <DataTable.Cell numeric>91</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Jared</DataTable.Cell>
+            <DataTable.Cell numeric>22</DataTable.Cell>
+            <DataTable.Cell numeric>81</DataTable.Cell>
+          </DataTable.Row>
         </DataTable>
       </ScrollView>
     </>
