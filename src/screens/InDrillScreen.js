b7c347a177d567ac82a7b7276972eb0c39e49a1c
George <gwmkwan@gmail.com>
2023-02-23 19:05:37 -0700

    feat: change drill timer speed for testing

--- src/screens/InDrillScreen.js
+++ src/screens/InDrillScreen.js
@@ -56,7 +56,7 @@ const InDrillScreen = ({ route }) => {
           return newVal;
         });
       },
-      selectedSeconds * 1000
+      selectedSeconds * 10
       // TODO change back to 1000 for final version
     );
     return () => Timer.clearInterval('soundTimer');
