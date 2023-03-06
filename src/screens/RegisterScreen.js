bd2edb23612d30dc1ac57a265ebb486e3cdcbbb3
George <gwmkwan@gmail.com>
2023-03-06 13:21:08 -0700

    chore: remove comments and unused file

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -15,31 +15,12 @@ import { passwordValidator } from '../helpers/passwordValidator';
 import { AuthContext } from '../providers/AuthProvider.js';
 
 function RegisterScreen({ navigation }) {
-  // const [name, setName] = useState({ value: '', error: '' })
-  // const [email, setEmail] = useState({ value: '', error: '' })
-  // const [password, setPassword] = useState({ value: '', error: '' })
-
-  // const onSignUpPressed = () => {
-  //   const nameError = nameValidator(name.value)
-  //   const emailError = emailValidator(email.value)
-  //   const passwordError = passwordValidator(password.value)
-  //   if (emailError || passwordError || nameError) {
-  //     setName({ ...name, error: nameError })
-  //     setEmail({ ...email, error: emailError })
-  //     setPassword({ ...password, error: passwordError })
-  //     return
-  //   }
-  // navigation.reset({
-  //   index: 0,
-  //   routes: [{ name: 'Dashboard' }],
-  // })
-  // }
   const { register, authErrorMessages } = useContext(AuthContext);
 
-  const [name, setName] = useState(''); // input field value cannot be null
-  const [email, setEmail] = useState(''); // input field value cannot be null
-  const [password, setPassword] = useState(''); // input field value cannot be null
-  const [age, setAge] = useState(''); // input field value cannot be null
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [age, setAge] = useState('');
 
   const [registrationRunning, setRegistrationRunning] = useState(false);
   const [errorMessage, setErrorMessage] = useState();
@@ -69,33 +50,19 @@ function RegisterScreen({ navigation }) {
       <BackButton goBack={navigation.goBack} />
       <Logo />
       <Header>Create Account</Header>
-      <TextInput
-        label="Name"
-        returnKeyType="next"
-        value={name}
-        // onChangeText={(text) => setName({ value: text, error: '' })}
-        onChangeText={(t) => setName(t)}
-        // error={!!name.error}
-        // errorText={name.error}
-      />
+      <TextInput label="Name" returnKeyType="next" value={name} onChangeText={(t) => setName(t)} />
       <TextInput
         label="Age"
         returnKeyType="next"
         value={age}
         keyboardType="numeric"
-        // onChangeText={(text) => setName({ value: text, error: '' })}
         onChangeText={(t) => setAge(t)}
-        // error={!!name.error}
-        // errorText={name.error}
       />
       <TextInput
         label="Email"
         returnKeyType="next"
         value={email}
-        // onChangeText={(text) => setEmail({ value: text, error: '' })}
         onChangeText={(e) => setEmail(e)}
-        // error={!!email.error}
-        // errorText={email.error}
         autoCapitalize="none"
         autoCompleteType="email"
         textContentType="emailAddress"
@@ -105,10 +72,7 @@ function RegisterScreen({ navigation }) {
         label="Password"
         returnKeyType="done"
         value={password}
-        // onChangeText={(text) => setPassword({ value: text, error: '' })}
         onChangeText={(e) => setPassword(e)}
-        // error={!!password.error}
-        // errorText={password.error}
         secureTextEntry
       />
       <Button mode="contained" onPress={handleButtonClick} style={{ marginTop: 24 }}>
